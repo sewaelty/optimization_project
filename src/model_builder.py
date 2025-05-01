41355e2ece0a892c9113d795bc5f8875ee0e93d4
sewaelty <severin.waelty@gmail.com>
2025-05-01 16:24:27 +0200

    test_sevi

--- src/model_builder.py
+++ src/model_builder.py
@@ -1,7 +1,16 @@
 from gurobipy import Model, GRB
 
+
 def build_model(data):
     model = Model("OptimizationModel")
+    # Inflexible variables
+
+    # Fixed flexible variables
+
+    # Heatpump
+
+    # Electriv Vehicle
+
     x = model.addVar(name="x", vtype=GRB.CONTINUOUS, lb=0)
     y = model.addVar(name="y", vtype=GRB.CONTINUOUS, lb=0)
     model.addConstr(x + y <= data["max"], name="c1")
