f98c86752eaf96bf4b1276e8717bd2c3cf2e2290
sewaelty <severin.waelty@gmail.com>
2025-05-01 16:34:17 +0200

    adding variables

--- src/model_builder.py
+++ src/model_builder.py
@@ -4,9 +4,14 @@ from gurobipy import Model, GRB
 def build_model(data):
     model = Model("OptimizationModel")
     # Inflexible variables
-
+    """lights = model.addVar(name="lights", vtype=GRB.BINARY, lb=0, ub=1)
+    fridge = model.addVar(name="fridge", vtype=GRB.BINARY, lb=0, ub=1)
+    tv = model.addVar(name="tv", vtype=GRB.BINARY, lb=0, ub=1)
+    stove = model.addVar(name="stove", vtype=GRB.BINARY, lb=0, ub=1)"""
     # Fixed flexible variables
-
+    """washing_machine = model.addVar(name="washing_machine", vtype=GRB.BINARY, lb=0, ub=1)
+    tumbler_dryer = model.addVar(name="tumbler_dryer", vtype=GRB.BINARY, lb=0, ub=1)
+    dishwasher = model.addVar(name="dishwasher", vtype=GRB.BINARY, lb=0, ub=1)"""
     # Heatpump
 
     # Electriv Vehicle
